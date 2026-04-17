7f8a6a677cd8add51a73bb0d8955bb5bf65e9ada
Dimitar Ivanov <dimitucha2004@gmail.com>
2026-04-17 15:40:14 +0200

    fix: add the useRef to the imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { CheckCircle2, XCircle } from "lucide-react";
 
 import { HeaderView } from "./components/HeaderView.tsx";
